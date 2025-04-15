5374a3681e3527e3b50eb2d73baaec702fa5a1fb
njones61 <njones61@gmail.com>
2025-04-15 12:37:44 -0600

    updates to slice and plot

--- main2.py
+++ main2.py
@@ -8,7 +8,8 @@ materials = data["materials"]
 piezo_line = data["piezo_line"]
 circle = data["circles"][0]  # or whichever one you want
 
-df = generate_slices(profile_lines, materials, circle, piezo_line=piezo_line)
+df, failure_surface = generate_slices(profile_lines, materials, circle, piezo_line=piezo_line)
 print(df)
+print(failure_surface)
 
-plot_slices(profile_lines, circle, df, piezo_line=piezo_line)
+# plot_slices(profile_lines, circle, df, piezo_line=piezo_line, failure_surface=failure_surface)

9a8de0593e795483b68c0260d9c83cd7f368b688
njones61 <njones61@gmail.com>
2025-04-16 14:41:57 -0600

    Added Janbu FE method and Morgenstern and Price

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from slice import generate_slices
 from fileio import load_globals
 from plot import plot_slices
 from utils import ground_surface
-from solve import oms, bishops, spencer
+from solve import oms, bishops, spencer, janbu_simple, janbu_corrected, morgenstern_price
 
 data = load_globals("input_template.xlsx")
 profile_lines = data["profile_lines"]
@@ -30,7 +30,31 @@ df.to_excel("slices.xlsx", index=False)
 #     print("Bishop's method did not converge.")
 # print(f"Factor of Safety = {FS:.3f}")
 
-FS, beta_deg = spencer(df)
-print(f"Spencer's Method: FS = {FS:.4f}, β = {beta_deg:.2f}°")
+# FS, beta_deg = spencer(df)
+# print(f"Spencer's Method: FS = {FS:.4f}, β = {beta_deg:.2f}°")
+
+# FS = janbu_simple(df)
+# print(f"Janbu FS = {FS:.4f}")
+
+# FS, lam, converged = janbu_corrected(df)
+# if converged:
+#     print(f"Janbu Corrected FS = {FS:.4f}, λ = {lam:.4f}")
+# else:
+#     print(f"Did not converge. Last FS = {FS:.4f}, λ = {lam:.4f}")
+
+
+
+
+# Default constant interslice force function (Janbu)
+FS, lam, converged = morgenstern_price(df)
+
+# With a half-sine function:
+# import math
+# FS, lam, converged = morgenstern_price(df, function=lambda x: math.sin(math.pi * x))
+
+if converged:
+    print(f"Morgenstern-Price FS = {FS:.4f}, λ = {lam:.4f}")
+else:
+    print(f"Did not converge. Last FS = {FS:.4f}, λ = {lam:.4f}")
 
 plot_slices(profile_lines, circle, df, piezo_line=piezo_line, failure_surface=arc, FS=FS)

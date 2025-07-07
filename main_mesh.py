5b1fd371ae124a77b6a32cd4633fd6c8f43a571c
njones61 <njones61@gmail.com>
2025-07-07 13:03:11 -0600

    succesfully meshed polygons created with build_polygon function.

--- main_mesh.py
+++ main_mesh.py
@@ -1,13 +1,23 @@
-from mesh import build_tri_mesh_with_regions, plot_mesh, plot_mesh_with_materials
+from fileio import load_globals
+
+from mesh import build_polygons, build_tri_mesh_with_regions, plot_mesh_with_materials, plot_polygons
+from plot import plot_inputs
 import numpy as np
 
-# Define two adjacent rectangles
-left_rect = [(0, 0), (1, 0), (1, 1), (0, 1)]
-right_rect = [(1, 0), (2, 0), (2, 1), (1, 1)]
+data = load_globals("inputs/slopes/input_template_dam2.xlsx")
 
-polygons = [left_rect, right_rect]
-region_ids = [1, 2]  # material IDs
-target_size = 0.1
+plot_inputs(data)
+
+polygons = build_polygons(data['profile_lines'], max_depth=data['max_depth'])
+
+plot_polygons(polygons)
+
+# build a list of region ids
+region_ids = [i for i in range(len(polygons))]
+
+# find the x-range of the ground_surface and use it to set the target size
+x_range = [min(x for x, _ in data['ground_surface'].coords), max(x for x, _ in data['ground_surface'].coords)]
+target_size = (x_range[1] - x_range[0]) / 150
 
 nodes, elements, mat_ids = build_tri_mesh_with_regions(polygons, region_ids, target_size)
 

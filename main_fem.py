4aaf8f12b49284e1dbbfd3a5f0b69fe5b20ceaae
njones61 <njones61@gmail.com>
2026-02-13 14:43:22 -0700

    added logic to save and read meshes automatically

--- main_fem.py
+++ main_fem.py
@@ -1,17 +1,34 @@
+from pathlib import Path
 from xslope.fem import build_fem_data, solve_fem, solve_ssrm
 from xslope.fileio import load_slope_data
-from xslope.mesh import build_polygons, build_mesh_from_polygons
+from xslope.mesh import build_polygons, build_mesh_from_polygons, export_mesh_to_json, import_mesh_from_json
 from xslope.plot import plot_inputs
 from xslope.plot_fem import plot_fem_results, plot_fem_data
 
 # Load Griffiths & Lane (1999) Example 1: homogeneous slope, c/γH=0.05, φ=20°
-slope_data = load_slope_data("docs/fem/files/xslope_griffiths1.xlsx")
+input_file = "docs/fem/files/xslope_griffiths1.xlsx"
+slope_data = load_slope_data(input_file)
 
 plot_inputs(slope_data, mode='fem', tab_loc='top')
 
-# Build mesh
-polygons = build_polygons(slope_data)
-mesh = build_mesh_from_polygons(polygons, target_size=2, element_type='quad8')
+# Check to see if the mesh file already exists
+input_path = Path(input_file)
+mesh_file = input_path.parent / f"{input_path.stem}_mesh.json"
+
+re_mesh = False
+target_size = 2
+element_type='quad8'
+save_mesh = True
+
+# Build the mesh if it doesn't exist or if re_mesh is True
+if re_mesh or not mesh_file.exists():
+    polygons = build_polygons(slope_data)
+    print(f"Building mesh with {len(polygons)} polygons.")
+    mesh = build_mesh_from_polygons(polygons, target_size=target_size, element_type=element_type)
+    if save_mesh:
+        export_mesh_to_json(mesh, mesh_file)
+else:
+    mesh = import_mesh_from_json(mesh_file)
 
 # Build FEM data
 fem_data = build_fem_data(slope_data, mesh)
--- main_seep.py
+++ main_seep.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pathlib import Path
 
 from xslope.fileio import load_slope_data, print_dictionary
 from xslope.mesh import build_polygons, build_mesh_from_polygons, export_mesh_to_json, import_mesh_from_json
@@ -6,25 +7,33 @@ from xslope.plot import plot_inputs, plot_mesh, plot_polygons, plot_polygons_sep
 from xslope.plot_seep import plot_seep_data, plot_seep_solution
 from xslope.seep import build_seep_data, run_seepage_analysis, save_seep_data_to_json, export_seep_solution
 
-path = "docs/seep/files/xslope_bear_creek.xlsx"
+input_file = "docs/seep/files/xslope_bear_creek.xlsx"
+input_path = Path(input_file)
+mesh_file = input_path.parent / f"{input_path.stem}_mesh.json"
 
-path_prefix = path.replace(".xlsx", "")  # make a copy of the path minus the extension for later use
-
-slope_data = load_slope_data(path)
+slope_data = load_slope_data(input_file)
 
 plot_inputs(slope_data, figsize=(12, 6), mode='seep', mat_table=False, tab_loc='top', save_png=True)
 
-polygons = build_polygons(slope_data, debug=True)
+re_mesh = False
+element_type = 'tri3'
+save_mesh = True
 
-plot_polygons(polygons, figsize=(12, 6), materials=slope_data['materials'], title="Material Zones", nodes=False, legend=False, save_png=True)
-# plot_polygons_separately(polygons, materials=slope_data['materials'], save_png=True)
+# Build the mesh if it doesn't exist or if re_mesh is True
+if re_mesh or not mesh_file.exists():
+    polygons = build_polygons(slope_data, debug=True)
+    # plot_polygons(polygons, figsize=(12, 6), materials=slope_data['materials'], title="Material Zones", nodes=False, legend=False, save_png=True)
+    # plot_polygons_separately(polygons, materials=slope_data['materials'], save_png=True)
 
-# find the x-range of the ground_surface and use it to set the target size
-x_range = [min(x for x, _ in slope_data['ground_surface'].coords), max(x for x, _ in slope_data['ground_surface'].coords)]
-target_size = (x_range[1] - x_range[0]) / 100
+    # find the x-range of the ground_surface and use it to set the target size
+    x_range = [min(x for x, _ in slope_data['ground_surface'].coords), max(x for x, _ in slope_data['ground_surface'].coords)]
+    target_size = (x_range[1] - x_range[0]) / 100
 
-# Build quadrilateral mesh
-mesh = build_mesh_from_polygons(polygons, target_size, 'tri3')
+    mesh = build_mesh_from_polygons(polygons, target_size, element_type)
+    if save_mesh:   
+        export_mesh_to_json(mesh, mesh_file)
+else:
+    mesh = import_mesh_from_json(mesh_file)
 
 # plot_mesh(mesh, materials=slope_data['materials'])
 
@@ -39,5 +48,5 @@ solution = run_seepage_analysis(seep_data, tol=1e-4)
 plot_seep_solution(seep_data, solution, figsize=(12, 6), variable="head", vectors=False, flowlines=True, 
                           mesh=False, levels=20, base_mat=3, fill_contours=False, phreatic=True, save_png=True)
 
-# export_mesh_to_json(mesh, path_prefix + "_mesh.json")
-# export_seep_solution(seep_data, solution, path_prefix + "_seep.csv")
+seep_file = input_path.parent / f"{input_path.stem}_seep.csv"
+export_seep_solution(seep_data, solution, seep_file)
